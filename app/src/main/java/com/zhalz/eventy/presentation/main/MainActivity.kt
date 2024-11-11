7e7fd865d696a1a0e385e3c8cae2ff8151eddfb9
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-11-11 08:35:25 +0700

    [C] Project setup

--- app/src/main/java/com/zhalz/eventy/presentation/main/MainActivity.kt
+++ app/src/main/java/com/zhalz/eventy/presentation/main/MainActivity.kt
@@ -1,20 +1,29 @@
-package com.zhalz.eventy
+package com.zhalz.eventy.presentation.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.zhalz.eventy.R
+import com.zhalz.eventy.base.BaseActivity
+import com.zhalz.eventy.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        initUI()
+    }
+
+    private fun initUI() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
 }
