090e24391c603414188088fa1a4b6e43178f76c5
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-11-11 15:23:25 +0700

    [C] Otp UI

--- app/src/main/java/com/zhalz/eventy/presentation/otp/OtpActivity.kt
+++ app/src/main/java/com/zhalz/eventy/presentation/otp/OtpActivity.kt
@@ -1,6 +1,9 @@
 package com.zhalz.eventy.presentation.otp
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -17,10 +20,29 @@ class OtpActivity : BaseActivity<ActivityOtpBinding, OtpViewModel>(R.layout.acti
 
         initUI()
 
+        val otpBoxes = (0 until binding.containerOtp.childCount).map { binding.containerOtp.getChildAt(it) as EditText }
+
+        otpBoxes.forEachIndexed { index, editText ->
+            editText.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                    if (s?.length == 1) {
+                        if (index < otpBoxes.size - 1) otpBoxes[index + 1].requestFocus()
+                        else {
+                            editText.clearFocus()
+                            if (otpBoxes.all { it.text.toString().isNotEmpty() }) binding.btnSend.requestFocus()
+                        }
+                    }
+                    else if (s?.isEmpty() == true && index > 0) otpBoxes[index - 1].requestFocus()
+                }
+
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            })
+        }
     }
 
     private fun initUI() {
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
