0270ed6adf2a84f64a93fce15f5301f6af1e6bd5
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-11-13 15:05:47 +0700

    [C] Home UI

--- app/src/main/java/com/zhalz/eventy/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/zhalz/eventy/presentation/home/HomeFragment.kt
@@ -10,6 +10,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+
     }
 
     override fun onDestroy() {
