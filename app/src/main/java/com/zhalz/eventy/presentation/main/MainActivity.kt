384bffd3d53afb0ee4351cba50282afa68a7950d
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-11-14 14:52:48 +0700

    [C] Bottom App Menu

--- app/src/main/java/com/zhalz/eventy/presentation/main/MainActivity.kt
+++ app/src/main/java/com/zhalz/eventy/presentation/main/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.zhalz.eventy.presentation.main
 
+import android.graphics.Color.TRANSPARENT
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -11,24 +11,30 @@ import com.crocodic.core.base.activity.NoViewModelActivity
 import com.crocodic.core.extension.tos
 import com.zhalz.eventy.R
 import com.zhalz.eventy.databinding.ActivityMainBinding
+import com.zhalz.eventy.utils.setStatusBarColor
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : NoViewModelActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private val appBarConfiguration by lazy { AppBarConfiguration(
-        setOf( R.id.nav_home ),
-        binding.drawerLayout
-    ) }
+    private val appBarConfiguration by lazy {
+        AppBarConfiguration(setOf(R.id.nav_home, R.id.nav_history, R.id.nav_notif), binding.drawerLayout)
+    }
 
-    private val navController by lazy { findNavController(R.id.nav_host) }
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment).navController
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        setStatusBarColor(TRANSPARENT)
 
         binding.activity = this
 
         setDrawer()
 
+        binding.bottomNav.setupWithNavController(navController)
     }
 
     private fun setDrawer() {
