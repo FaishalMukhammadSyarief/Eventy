5d922a321f8e23db9ef494d7ca0ec765c141e626
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-11-17 22:58:02 +0700

    [C] Division UI

--- app/src/main/java/com/zhalz/eventy/presentation/division/task/TaskFragment.kt
+++ app/src/main/java/com/zhalz/eventy/presentation/division/task/TaskFragment.kt
@@ -6,21 +6,18 @@ import android.view.View
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.crocodic.core.base.adapter.ReactiveListAdapter
-import com.crocodic.core.extension.openActivity
 import com.zhalz.eventy.R
 import com.zhalz.eventy.base.BaseFragment
 import com.zhalz.eventy.databinding.FragmentTaskBinding
 import com.zhalz.eventy.databinding.ItemTaskBinding
 import com.zhalz.eventy.domain.model.Division
 import com.zhalz.eventy.domain.model.Task
-import com.zhalz.eventy.presentation.profile.ProfileActivity
 import com.zhalz.eventy.utils.Constanta.Parcel.EXTRA_DIVISION
-import com.zhalz.eventy.utils.Constanta.Parcel.EXTRA_TASK
 
 class TaskFragment : BaseFragment<FragmentTaskBinding>(R.layout.fragment_task) {
 
     private val taskAdapter by lazy {
-        ReactiveListAdapter<ItemTaskBinding, Task>(R.layout.item_task).initItem { _, data -> toDetail(data) }
+        ReactiveListAdapter<ItemTaskBinding, Task>(R.layout.item_task)
     }
 
     val division: Division? by lazy {
@@ -41,7 +38,7 @@ class TaskFragment : BaseFragment<FragmentTaskBinding>(R.layout.fragment_task) {
 
     }
 
-    private fun toDetail(task: Task) = context?.openActivity<ProfileActivity> { putExtra(EXTRA_TASK, task) }
+//    private fun toDetail(task: Task) = context?.openActivity<ProfileActivity> { putExtra(EXTRA_TASK, task) }
 
     override fun onDestroyView() {
         super.onDestroyView()
