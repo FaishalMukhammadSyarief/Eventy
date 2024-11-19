473527455d3d4265ce29ff632be62447814f4cad
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-11-19 15:07:38 +0700

    [C] Splash screen

--- app/src/main/java/com/zhalz/eventy/presentation/splash/SplashCustomActivity.kt
+++ app/src/main/java/com/zhalz/eventy/presentation/splash/SplashCustomActivity.kt
@@ -1,20 +1,26 @@
-package com.zhalz.eventy.presentation
+package com.zhalz.eventy.presentation.splash
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.postDelayed
 import com.crocodic.core.base.activity.NoViewModelActivity
+import com.crocodic.core.extension.openActivity
 import com.zhalz.eventy.R
 import com.zhalz.eventy.databinding.ActivitySplashBinding
+import com.zhalz.eventy.presentation.landing.LandingActivity
 
-class SplashActivity : NoViewModelActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+class SplashCustomActivity : NoViewModelActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         initUI()
+
+        window.decorView.postDelayed(2000) {
+            openActivity<LandingActivity> { finish() }
+        }
 
     }
 
