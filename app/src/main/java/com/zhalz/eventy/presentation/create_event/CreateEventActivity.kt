b0e2c924a01d0a874d4c80e6711cb4d6be766da8
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-11-21 15:28:36 +0700

    [C] Date Picker

--- app/src/main/java/com/zhalz/eventy/presentation/create_event/CreateEventActivity.kt
+++ app/src/main/java/com/zhalz/eventy/presentation/create_event/CreateEventActivity.kt
@@ -2,9 +2,14 @@ package com.zhalz.eventy.presentation.create_event
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import com.google.android.material.datepicker.MaterialDatePicker.Builder.datePicker
+import com.google.android.material.datepicker.MaterialDatePicker.todayInUtcMilliseconds
 import com.zhalz.eventy.R
 import com.zhalz.eventy.base.BaseActivity
 import com.zhalz.eventy.databinding.ActivityCreateEventBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class CreateEventActivity : BaseActivity<ActivityCreateEventBinding, CreateEventViewModel>(R.layout.activity_create_event) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +23,29 @@ class CreateEventActivity : BaseActivity<ActivityCreateEventBinding, CreateEvent
             true
         }
 
+        binding.etStartDate.setOnClickListener {
+            showMaterialDatePicker { binding.etStartDate.setText(it) }
+        }
+
+        binding.etDueDate.setOnClickListener {
+            showMaterialDatePicker { binding.etDueDate.setText(it) }
+        }
+
+    }
+
+    private fun showMaterialDatePicker(onDateSelected: (String) -> Unit) {
+        val datePicker = datePicker()
+            .setTitleText("Select Start Date")
+            .setSelection(todayInUtcMilliseconds())
+            .build()
+
+        datePicker.addOnPositiveButtonClickListener { selection ->
+            val formatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                .format(Date(selection))
+            onDateSelected(formatter)
+        }
+
+        datePicker.show(supportFragmentManager, "MATERIAL_DATE_PICKER")
     }
 
 }
