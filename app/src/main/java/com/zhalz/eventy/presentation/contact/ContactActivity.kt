b6257d5e714c445ad4a8d91d9065e12470945767
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-12-07 11:43:57 +0700

    [C] Add collaborator dialog on contact

--- app/src/main/java/com/zhalz/eventy/presentation/contact/ContactActivity.kt
+++ app/src/main/java/com/zhalz/eventy/presentation/contact/ContactActivity.kt
@@ -7,6 +7,7 @@ import com.google.android.material.tabs.TabLayoutMediator
 import com.zhalz.eventy.R
 import com.zhalz.eventy.databinding.ActivityContactBinding
 import com.zhalz.eventy.presentation.adapter.ContactPagerAdapter
+import com.zhalz.eventy.presentation.dialog.AddPeopleFragment
 
 class ContactActivity : NoViewModelActivity<ActivityContactBinding>(R.layout.activity_contact) {
 
@@ -28,6 +29,13 @@ class ContactActivity : NoViewModelActivity<ActivityContactBinding>(R.layout.act
                 1 -> tab.text = getString(R.string.collaborator)
             }
         }.attach()
+
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.menu_add_friend -> AddPeopleFragment().show(supportFragmentManager, AddPeopleFragment::class.java.simpleName)
+            }
+            true
+        }
     }
 
 }
