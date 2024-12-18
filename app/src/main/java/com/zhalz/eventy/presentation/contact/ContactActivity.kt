6a074794a53e23d248b29e6467f36f97ed762638
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-12-18 11:01:34 +0700

    [U] Pager in contact with extension function

--- app/src/main/java/com/zhalz/eventy/presentation/contact/ContactActivity.kt
+++ app/src/main/java/com/zhalz/eventy/presentation/contact/ContactActivity.kt
@@ -3,11 +3,12 @@ package com.zhalz.eventy.presentation.contact
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import com.crocodic.core.base.activity.NoViewModelActivity
-import com.google.android.material.tabs.TabLayoutMediator
 import com.zhalz.eventy.R
 import com.zhalz.eventy.databinding.ActivityContactBinding
-import com.zhalz.eventy.presentation.adapter.ContactPagerAdapter
+import com.zhalz.eventy.presentation.contact.collaborator.CollaboratorFragment
+import com.zhalz.eventy.presentation.contact.friend.FriendFragment
 import com.zhalz.eventy.presentation.dialog.add_people.AddPeopleFragment
+import com.zhalz.eventy.utils.setupWithTabLayout
 
 class ContactActivity : NoViewModelActivity<ActivityContactBinding>(R.layout.activity_contact) {
 
@@ -21,14 +22,12 @@ class ContactActivity : NoViewModelActivity<ActivityContactBinding>(R.layout.act
     }
 
     private fun initUI() {
-        /** == VIEW PAGER == **/
-        binding.viewPager.adapter = ContactPagerAdapter(this)
-        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
-            when (position) {
-                0 -> tab.text = getString(R.string.friend2)
-                1 -> tab.text = getString(R.string.collaborator)
-            }
-        }.attach()
+        binding.viewPager.setupWithTabLayout(
+            this,
+            binding.tabLayout,
+            listOf(FriendFragment(), CollaboratorFragment()),
+            listOf(getString(R.string.friend2), getString(R.string.collaborator))
+        )
 
         binding.toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
