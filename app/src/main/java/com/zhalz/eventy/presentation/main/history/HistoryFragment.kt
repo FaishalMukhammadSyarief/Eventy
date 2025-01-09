6620969229f74a4cf2958fc1f7c20d4b9c7fb061
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2025-01-09 14:00:12 +0700

    [U] History -> Event Frgament

--- app/src/main/java/com/zhalz/eventy/presentation/main/history/HistoryFragment.kt
+++ app/src/main/java/com/zhalz/eventy/presentation/main/history/HistoryFragment.kt
@@ -2,18 +2,15 @@ package com.zhalz.eventy.presentation.main.history
 
 import android.os.Bundle
 import android.view.View
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.crocodic.core.base.adapter.ReactiveListAdapter
-import com.crocodic.core.extension.openActivity
 import com.zhalz.eventy.R
 import com.zhalz.eventy.base.BaseFragment
 import com.zhalz.eventy.data.eventList
 import com.zhalz.eventy.databinding.FragmentHistoryBinding
 import com.zhalz.eventy.databinding.ItemHistoryBinding
 import com.zhalz.eventy.domain.model.Event
-import com.zhalz.eventy.presentation.event.EventActivity
-import com.zhalz.eventy.utils.Constanta.Parcel.EXTRA_EVENT
+import com.zhalz.eventy.utils.extension.addDivider
+import com.zhalz.eventy.utils.extension.navigate
 
 class HistoryFragment : BaseFragment<FragmentHistoryBinding>(R.layout.fragment_history) {
 
@@ -24,18 +21,17 @@ class HistoryFragment : BaseFragment<FragmentHistoryBinding>(R.layout.fragment_h
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding?.historyAdapter = historyAdapter
-
-        DividerItemDecoration(requireContext(), LinearLayoutManager(requireContext()).orientation).also {
-            binding?.rvHistory?.addItemDecoration(it)
-        }
+        initUI()
 
         historyAdapter.submitList(eventList)
-        binding?.historyAdapter = historyAdapter
-
     }
 
-    private fun toDetail(event: Event) = context?.openActivity<EventActivity> { putExtra(EXTRA_EVENT, event) }
+    private fun initUI() = binding?.rvHistory?.apply {
+        addDivider(requireContext())
+        adapter = historyAdapter
+    }
+
+    private fun toDetail(event: Event) = HistoryFragmentDirections.actionHistoryToEvent(event).navigate(this)
 
     override fun onDestroyView() {
         super.onDestroyView()
