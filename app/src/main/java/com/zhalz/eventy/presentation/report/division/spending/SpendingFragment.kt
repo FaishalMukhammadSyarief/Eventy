5fdeff3e3f8503c82ed89a7253a5211ed65b6d98
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2025-01-11 16:41:45 +0700

    [C] Spending

--- app/src/main/java/com/zhalz/eventy/presentation/report/division/spending/SpendingFragment.kt
+++ app/src/main/java/com/zhalz/eventy/presentation/report/division/spending/SpendingFragment.kt
@@ -1,23 +1,18 @@
 package com.zhalz.eventy.presentation.report.division.spending
 
-import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.navArgs
 import com.crocodic.core.base.adapter.ReactiveListAdapter
 import com.zhalz.eventy.R
 import com.zhalz.eventy.base.BaseFragment
 import com.zhalz.eventy.databinding.FragmentSpendingBinding
 import com.zhalz.eventy.databinding.ItemSpendingBinding
-import com.zhalz.eventy.domain.model.Division
 import com.zhalz.eventy.domain.model.Spending
-import com.zhalz.eventy.utils.Constanta.Parcel.EXTRA_DIVISION
 
 class SpendingFragment : BaseFragment<FragmentSpendingBinding>(R.layout.fragment_spending) {
 
-    private val division: Division? by lazy {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) requireActivity().intent.getParcelableExtra(EXTRA_DIVISION, Division::class.java)
-        else @Suppress("DEPRECATION") requireActivity().intent.getParcelableExtra(EXTRA_DIVISION)
-    }
+    private val args by navArgs<SpendingFragmentArgs>()
 
     private val spendingAdapter by lazy {
         ReactiveListAdapter<ItemSpendingBinding, Spending>(R.layout.item_spending)
@@ -27,7 +22,7 @@ class SpendingFragment : BaseFragment<FragmentSpendingBinding>(R.layout.fragment
         super.onViewCreated(view, savedInstanceState)
 
         binding?.spendingAdapter = spendingAdapter
-        binding?.division = division
+        binding?.division = args.division
 
         val spendingList = listOf(
             Spending(123, "Makanan", "22 Agustus 2024", 75000),
