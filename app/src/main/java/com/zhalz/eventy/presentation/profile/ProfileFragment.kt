2a805e8646906744653c4a20d811a69b64cba7b9
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2025-01-14 13:25:49 +0700

    [C] Profile Banner

--- app/src/main/java/com/zhalz/eventy/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/zhalz/eventy/presentation/profile/ProfileFragment.kt
@@ -6,8 +6,6 @@ import com.zhalz.eventy.R
 import com.zhalz.eventy.base.BaseFragment
 import com.zhalz.eventy.data.user
 import com.zhalz.eventy.databinding.FragmentProfileBinding
-import com.zhalz.eventy.utils.extension.addMenu
-import com.zhalz.eventy.utils.extension.clearMenu
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
 
@@ -16,20 +14,8 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_p
 
         bind.fragment = this
         bind.person = user
+        bind.isUser = user.id == 1234567
 
-    }
-
-    fun toEdit() {
-        bind.isEdit = true
-        addMenu(R.menu.menu_profile) {
-            if (it.itemId == R.id.menu_check) { updateProfile(); true}
-            else false
-        }
-    }
-
-    private fun updateProfile() {
-        bind.isEdit = false
-        clearMenu()
     }
 
 }
