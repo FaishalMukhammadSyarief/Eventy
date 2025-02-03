d154e1913f7341d0fb99a453230ece1b0710ed32
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2025-02-03 19:21:17 +0700

    [U] Pre-populate task adapter

--- app/src/main/java/com/zhalz/eventy/presentation/division/task/TaskFragment.kt
+++ app/src/main/java/com/zhalz/eventy/presentation/division/task/TaskFragment.kt
@@ -1,7 +1,5 @@
 package com.zhalz.eventy.presentation.division.task
 
-import android.os.Build.VERSION.SDK_INT
-import android.os.Build.VERSION_CODES.TIRAMISU
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -10,23 +8,17 @@ import com.crocodic.core.base.adapter.ReactiveListAdapter
 import com.crocodic.core.extension.openActivity
 import com.zhalz.eventy.R
 import com.zhalz.eventy.base.BaseFragment
+import com.zhalz.eventy.data.taskList
 import com.zhalz.eventy.databinding.FragmentTaskBinding
 import com.zhalz.eventy.databinding.ItemTaskBinding
-import com.zhalz.eventy.domain.model.Division
 import com.zhalz.eventy.domain.model.Task
 import com.zhalz.eventy.presentation.task.TaskActivity
-import com.zhalz.eventy.utils.Constanta.Parcel.EXTRA_DIVISION
 import com.zhalz.eventy.utils.Constanta.Parcel.EXTRA_TASK
 
 class TaskFragment : BaseFragment<FragmentTaskBinding>(R.layout.fragment_task) {
 
     private val taskAdapter by lazy {
         ReactiveListAdapter<ItemTaskBinding, Task>(R.layout.item_task).initItem { _, data -> toDetail(data) }
-    }
-
-    val division: Division? by lazy {
-        if (SDK_INT >= TIRAMISU) activity?.intent?.getParcelableExtra(EXTRA_DIVISION, Division::class.java)
-        else @Suppress("DEPRECATION") activity?.intent?.getParcelableExtra(EXTRA_DIVISION)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -38,7 +30,7 @@ class TaskFragment : BaseFragment<FragmentTaskBinding>(R.layout.fragment_task) {
             binding?.rvContact?.addItemDecoration(it)
         }
 
-        taskAdapter.submitList(division?.task)
+        taskAdapter.submitList(taskList)
 
     }
 
